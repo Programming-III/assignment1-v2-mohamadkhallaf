a38b5c3b6422f10bd6a7b8a8739fb425a68aa870
mohamadkhallaf <mohamad.khallaf@student.giu-uni.de>
2025-11-11 09:46:35 +0200

    Create Instructor class in Student.h
    Added Instructor class that inherits from Person with department and experience attributes.

--- Student.h
+++ Student.h
@@ -1,21 +1,31 @@
-#ifndef STUDENT_H
-#define STUDENT_H
-
-#include "Person.h"
+#ifndef INSTRUCTOR_H
+#define INSTRUCTOR_H
 #include <string>
-#using namespace std;
-//#write student class here
-
-
-
-
-
-
-
-
-
-
-
-
+#include "Person.h"
+using namespace std;
+class Instructor:Person{
+private:
+string department;
+int experienceYears;
+public:
+void display();
+Instructor();
+Instructor(string name,int id,string department,int experienceYears);
+string getDepartment(){
+    return department;
+}
+void setDepartment(string name)
+{
+    this->department=name;
+}
+int getExperienceYears(){
+    return experienceYears;
+}
+void setExperienceYears(int experienceYears){
+    this->experienceYears=experienceYears;
+}
+};
 
 #endif
+
+
