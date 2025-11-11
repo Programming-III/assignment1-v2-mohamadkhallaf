58229b250ad555bacf72cf95151f38423d41c67c
mohamadkhallaf <mohamad.khallaf@student.giu-uni.de>
2025-11-11 09:49:34 +0200

    Implement Instructor class with attributes and methods

--- Instructor.h
+++ Instructor.h
@@ -1,19 +1,30 @@
 #ifndef INSTRUCTOR_H
 #define INSTRUCTOR_H
-
+#include <string>
 #include "Person.h"
 using namespace std;
-//#write Instructor class here
-
-
-
-
-
-
-
-
-
-
-
+class Instructor:Person{
+private:
+string department;
+int experienceYears;
+public:
+void display();
+Instructor();
+Instructor(string name,int id,string department,int experienceYears);
+string getDepartment(){
+    return department;
+}
+void setDepartment(string name)
+{
+    this->department=name;
+}
+int getExperienceYears(){
+    return experienceYears;
+}
+void setExperienceYears(int experienceYears){
+    this->experienceYears=experienceYears;
+}
+};
 
 #endif
+
