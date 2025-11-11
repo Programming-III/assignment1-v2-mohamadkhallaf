cdad7e127e188a433f41dcc05a3589390f9cd7cb
mohamadkhallaf <mohamad.khallaf@student.giu-uni.de>
2025-11-11 09:54:04 +0200

    Implement Person class with basic attributes and methods

--- Person.h
+++ Person.h
@@ -1,20 +1,28 @@
 #ifndef PERSON_H
 #define PERSON_H
-
 #include <string>
 using namespace std;
-
-//#write Person class here
-
-
-
-
-
-
-
-
-
-
-
-
+class Person{
+private:
+string name;
+int id;
+public:
+Person();
+~Person();
+Person(string name,int id);
+void display();
+void setid(int id){
+    this->id=id;
+}
+int getid(){
+    return id;
+}
+string getName(){
+    return name;
+}
+void setName(string name)
+{
+    this->name=name;
+}
+};
 #endif
