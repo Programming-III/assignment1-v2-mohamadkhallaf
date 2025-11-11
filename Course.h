6ba9b0ae80a1fc72d8f16af5e43c43190d5c774c
mohamadkhallaf <mohamad.khallaf@student.giu-uni.de>
2025-11-11 09:59:56 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -4,19 +4,22 @@
 #include <string>
 #include "Student.h"
 using namespace std;
-//#write your code here
 
+class Course {
+private:
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
 
+public:
+Course();
+    Course(string code, string name, int max);               
+    ~Course();                                               
 
-
-
-
-
-
-
-
-
-
-
+    void addStudent(const Student& s);                      
+    void displayCourseInfo();                               
+};
 
 #endif
