b16747d03c38dfe37075cfc82bb9f9b7524a7dea
mohamadkhallaf <mohamad.khallaf@student.giu-uni.de>
2025-11-11 10:00:58 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,45 +4,40 @@
 #include "Student.h"
 #include "Instructor.h"
 #include "Course.h"
-
-
-using namespace std;
-
 // ==================== Person Class Implementation =========================
-
-
-
-
-
-
-
-
-// ==================== Student Class Implementation ====================
-
-
-
-
 
 
 // ==================== Instructor Class Implementation ====================
 
+class Instructor : public Person {
 
+    Instructor:: Instructor() : Person() {
+        setDepartment("");
+        setExperienceYears(0);
+    }
 
+    Instructor(string n, int i, string d, int exp) : Person(n, i) {
+        department = d;
+        experienceYears = exp;
+    }
 
+    void display() const {
+        cout << "Instructor Info:" << endl;
+        cout << "Name: " << name << endl;
+        cout << "Department: " << department << endl;
+        cout << "Experience: " << experienceYears << " years" << endl;
+    }
+};
 
-
-// ==================== Course Class Implementation ====================
-
-
-
-
-
-
-
-
-// ==================== Main Function ====================
-int main() {
-   
-    
-    return 0;
+class Course{
+        ~Course() {
+        delete[] students;
+    }
 }
+
+using namespace std;
+int main() {
+ return 0;
+}
+
+//g++ main.cpp -o main.exe
